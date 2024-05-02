117f482fe33baab4c793579e5746f73c16534994
Fastorcito <fabian46823@gmail.com>
2024-05-02 16:13:55 -0500

    Complete Product CRUD features

--- modulo_inventario/forms.py
+++ modulo_inventario/forms.py
@@ -18,3 +18,21 @@ class ProductForm(forms.ModelForm):
             'price': 'Precio',
             'category': 'Categoría',
         }
+
+class ProductUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'category']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa el nombre del producto'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Ingresa la descripción del producto'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa el precio (00.00)'}),
+            'category': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Selecciona la categoría'}),
+        }
+        labels = {
+            'name': 'Nombre',
+            'description': 'Descripción',
+            'price': 'Precio',
+            'category': 'Categoría',
+        }
+
--- modulo_inventario/views.py
+++ modulo_inventario/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Product, Inventory, Location
-from .forms import ProductForm
+from .forms import ProductForm, ProductUpdateForm
 
 # Create your views here.
 def index(request):
@@ -18,32 +18,28 @@ def product_create(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('products/product_list')
+            return redirect('products_list')
     else:
         form = ProductForm()
     products = Product.objects.all()
     context = {'products': products, 'form': form}
     return render(request, 'products/product_list.html', context)
 
-# def product_edit(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     if request.method == 'POST':
-#         form = ProductForm(request.POST, instance=product)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('products/product_list')
-#     else:
-#         form = ProductForm(instance=product)
-#     products = Product.objects.all()
-#     context = {'products': products, 'form': form}
-#     return render(request, 'products/product_list.html', context)
+def product_update(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductUpdateForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products_list')
+    else:
+        form = ProductUpdateForm(instance=product)
+    
+    context = {'form': form, 'product_id': product_id}
 
-# def product_delete(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     if request.method == 'POST':
-#         product.delete()
-#         return redirect('product_list')
-#     products = Product.objects.all()
-#     form = ProductForm()
-#     context = {'products': products, 'form': form, 'product_to_delete': product}
-#     return render(request, 'products/product_list.html', context)
+    return render(request, 'products/product_update.html',context)
+
+def product_delete(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product.delete()
+    return redirect('products_list')
